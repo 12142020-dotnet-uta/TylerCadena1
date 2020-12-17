00534efc040be565adc3f04b308a5651bfe9609c
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-12-17 14:41:08 -0800

    in-memdb works!

--- Demos/Week1/AdvancedRps/Rps.Storage/Contexts/InMemoryContext.cs
+++ Demos/Week1/AdvancedRps/Rps.Storage/Contexts/InMemoryContext.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 using Rps.Storage.Abstracts;
 
 namespace Rps.Storage.Contexts {
 	public class InMemoryContext : AContext {
 		public InMemoryContext() : base() {}
+		protected override void OnConfiguring(DbContextOptionsBuilder options) {
+			// options.UseInMemoryDatabase("rps-testing");
+		}
 	}
 }
--- Demos/Week1/AdvancedRps/Rps.Testing/UnitTest.cs
+++ Demos/Week1/AdvancedRps/Rps.Testing/UnitTest.cs
@@ -1,11 +1,14 @@
 using System;
 using Xunit;
 
+using Rps.Storage.Services;
+
 namespace Rps.Testing {
 	public class UnitTest {
 		[Fact]
 		public void Test() {
-
+			TestingService service = new TestingService();
+			Assert.NotNull(service);
 		}
 	}
 }
