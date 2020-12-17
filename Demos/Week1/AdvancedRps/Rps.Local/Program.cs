9f041319df7630669dc5544826ff009687b58051
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-12-16 21:43:49 -0800

    scaffholding for rps-local

--- Demos/Week1/AdvancedRps/Rps.Local/Program.cs
+++ Demos/Week1/AdvancedRps/Rps.Local/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
+using Rps.Storage.Models;
+using Rps.Storage.Services;
+
 namespace Rps.Local {
+	internal enum Move {
+		Rock = 1,
+		Paper,
+		Scissors
+	}
 	internal class Program {
+		private static readonly ConsoleService db = new ConsoleService();
+		public static void RunGame() {
+			// Blah blah blah
+		}
 		public static void Main(string[] args) {
-			Console.WriteLine("Hello, world!");
+			Console.WriteLine("Welcome to Rock-Paper-Scissors Advanced!\n");
+
+			while (true) {
+				RunGame();
+
+				Console.WriteLine("Continue playing? (Y/N)");
+				ConsoleKeyInfo k = Console.ReadKey(true);
+				if (k.KeyChar != 'y') break;
+			}
+			Console.WriteLine("Goodbye!");
 		}
 	}
 }
