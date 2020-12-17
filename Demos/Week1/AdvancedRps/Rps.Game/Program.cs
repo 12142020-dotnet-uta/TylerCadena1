3af37069a864fcdbddf2a10c8973e1db07cd626c
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-12-16 20:49:17 -0800

    Rps.Game -> Rps.Local

--- Demos/Week1/AdvancedRps/Rps.Game/Program.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Rps.Game {
-	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello, world!");
-		}
-	}
-}
---
+++ Demos/Week1/AdvancedRps/Rps.Local/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Rps.Local {
+	internal class Program {
+		public static void Main(string[] args) {
+			Console.WriteLine("Hello, world!");
+		}
+	}
+}
