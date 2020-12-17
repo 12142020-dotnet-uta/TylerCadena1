de51f0f3dc43eda679028344907be6f45248e669
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-12-17 12:46:40 -0800

    sqlite script no longer needs argument

--- Demos/Week1/AdvancedRps/migrate-sqlite.sh
+++ Demos/Week1/AdvancedRps/migrate-sqlite.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-dotnet-ef migrations add SqliteMigration${1} -p Rps.Storage/Rps.Storage.csproj -s Rps.Local/Rps.Local.csproj -c SqliteContext
+if [ ! -z "${1}" ]; then
+	dotnet-ef migrations add SqliteMigration${1} -p Rps.Storage/Rps.Storage.csproj -s Rps.Local/Rps.Local.csproj -c SqliteContext
+fi
 dotnet-ef database update -p Rps.Storage/Rps.Storage.csproj -s Rps.Local/Rps.Local.csproj -c SqliteContext
